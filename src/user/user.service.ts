0616ee68340bf3ab7156fdc48efb54efa0b3df12
Laysa Fontoura <laysafontoura@gmail.br>
2025-05-31 15:09:14 -0300

    feat(user): encrypt password using bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { Prisma, User } from 'generated/prisma';
 import { PrismaService } from '../database/prisma.service';
 
@@ -8,8 +9,9 @@ export class UserService {
   private readonly prismaService: PrismaService;
 
   async createUser(data: Prisma.UserCreateInput) {
+    const hash = await bcrypt.hash(data.password, 10);
     return this.prismaService.user.create({
-      data,
+      data: { ...data, password: hash },
     });
   }
 
