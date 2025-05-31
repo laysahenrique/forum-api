98d67b51f30561eff1b0ebad5010a53929b4bc6b
Laysa Fontoura <laysafontoura@gmail.br>
2025-05-31 14:45:28 -0300

    feat(user): add Prisma schema, model, controller and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Prisma, User as UserModel } from 'generated/prisma';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('signup')
+  async signup(@Body() userData: Prisma.UserCreateInput): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma, User } from 'generated/prisma';
+import { PrismaService } from '../database/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject()
+  private readonly prismaService: PrismaService;
+
+  async createUser(data: Prisma.UserCreateInput) {
+    return this.prismaService.user.create({
+      data,
+    });
+  }
+
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prismaService.user.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prismaService.user.delete({
+      where,
+    });
+  }
+}
