74819eeb63cc0ded3a76d2bf0f1c1d084cea7c79
Laysa Fontoura <laysafontoura@gmail.br>
2025-05-31 22:45:28 -0300

    feat(user): add get by id, update, and delete functions

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { Prisma, User as UserModel } from 'generated/prisma';
 import { UserService } from './user.service';
 
@@ -6,8 +14,29 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('signup')
+  @Post()
   async signup(@Body() userData: Prisma.UserCreateInput): Promise<UserModel> {
     return this.userService.createUser(userData);
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel | null> {
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() userData: Prisma.UserUpdateInput,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
   }
 }
